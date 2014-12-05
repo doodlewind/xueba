ecc2d4f270b0862df133fb997ca4cd2de31e2b6c
ewind <ewind@mail.ustc.edu.cn>
2014-12-05 18:46:52 +0800

    basic http client

--- demo.js
+++ demo.js
@@ -1,8 +1,32 @@
 $(document).ready(function() {
+    $('.btn').removeAttr('disabled');
+    $('#ustcId').focus();
+
     $("#mainPanel").hide();
     $("form").submit(function(e) {
         e.preventDefault();
-        $("#loginForm").hide(500);
-        $("#mainPanel").show(500);
+
+        var id = $('#ustcId').val().toUpperCase();
+        var password = $('#password').val();
+        var loginData = {"ustc_id": id, "password": password};
+        var btn = $('.btn');
+        btn.attr('disabled', true);
+
+        $.ajax({
+            type: 'POST',
+            url: '/login',
+            data: loginData,
+            dataType: 'json',
+            success: function(json, textStatus) {
+                $("#loginForm").hide(500);
+                $("#mainPanel").show(500);
+                $('name').html(json['name']);
+                $('cookie').html(json['cookie']);
+            },
+            error: function(xhr, textStatus, error) {
+                btn.removeAttr("disabled");
+                alert("Sorry, login failed.");
+            }
+        });
     });
 });
--- lup-demo.py
+++ lup-demo.py
@@ -1,12 +1,32 @@
 #coding=utf-8
 from tornado.ioloop import IOLoop
+from tornado import httpclient
 from tornado import web
+from tornado import gen
 import os
 import sys
 
 
+class LoginHandler(web.RequestHandler):
+
+    @gen.coroutine
+    def post(self):
+
+        ecard_url = "http://ecard.ustc.edu.cn"
+
+        visit_index = httpclient.HTTPRequest(
+            url=ecard_url, method='GET'
+        )
+        response = yield gen.Task(client.fetch, visit_index)
+        cookie = response.headers['Set-Cookie'].split(';')[0]
+
+        self.write({'cookie': cookie})
+        self.finish()
+
+
 def make_app():
     return web.Application([
+        (r"/login", LoginHandler),
         (r"/", web.RedirectHandler, {'url': 'index.html'}),
         (r"/(.*)", web.StaticFileHandler, {'path': os.getcwd()}),
     ])
@@ -14,7 +34,9 @@ def make_app():
 
 if __name__ == "__main__":
 
-    # start up
+    # prepare client
+    client = httpclient.AsyncHTTPClient()
+
     app = make_app()
     app.listen(sys.argv[1])
     IOLoop.instance().start()
